4a75d1554895a9525ef33fb02a36d255fe3c991b
CountTheSevens <dev.middaugh@gmail.com>
2016-07-19 21:45:53 -0700

    Added listPractice, magic8Ball, and stringPractice py files

---
+++ StringPractice.py
@@ -0,0 +1,22 @@
+# This file will take a string and show various methods to manipulate the characters
+
+myString = "This is my string"
+
+# Print string, basic
+print(myString)
+
+# Print upper and lower case of myString
+print(myString.upper())
+print(myString.lower())
+
+# Printng with different justifications
+print("Left Justify:")
+print(myString.ljust(30, '-'))
+print("Right Justify:")
+print(myString.rjust(30, '-'))
+print("Center Justify:")
+print(myString.center(30, '='))
+
+
+# Print string vertically
+
